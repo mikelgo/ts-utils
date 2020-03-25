fe112e55007f36ed4de682798c3579b3d6f2bd48
michago <m.gonser@icloud.com>
2020-03-25 23:10:55 +0100

    further config and add unit tests

---
+++ src/test-utils.ts
@@ -0,0 +1,31 @@
+/**
+ * Test helpers
+ */
+export const primitiveValues = {
+  number: 42,
+  null: null,
+  undefined: undefined,
+  string: 'some string',
+  true: true,
+  false: false
+};
+
+export const primitiveValuesAsArray = Array.from(Object.values(primitiveValues));
+
+export const objectValues = {
+  empty: {},
+  any: { id: 1, name: 'test' }
+};
+
+export const objectValuesAsArray = Array.from(Object.values(objectValues));
+
+export const arrayValues = {
+  empty: [],
+  strings: ['a', 'b', 'c'],
+  numbers: [1, 2, 3],
+  boolean: [true, false],
+  null: [null],
+  undefined: [undefined]
+};
+
+export const arrayValuesAsArray = Array.from(Object.values(arrayValues));
