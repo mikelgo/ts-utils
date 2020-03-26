46a1d0930e93ed16ef949ac9c2ae2bf59179928f
michago <m.gonser@icloud.com>
2020-03-26 20:13:48 +0100

    fixed tests

--- __tests__/array-utils.test.ts
+++ __tests__/array-utils.test.ts
@@ -1,132 +1,119 @@
-// import {
-//   isArray,
-//   isEmptyArray,
-//   isNotArray,
-//   isNotEmptyArray,
-//   allValuesInArrayAreEqual,
-//   allValuesInArrayAreNotEqual
-// } from '../src/array-utils';
-// import {
-//   arrayValues,
-//   arrayValuesAsArray,
-//   primitiveValues,
-//   primitiveValuesAsArray,
-//   objectValues,
-//   objectValuesAsArray
-// } from './test-utils';
-// describe('[Array utils]', () => {
-//   it('isArray() should return true if the input is an array', () => {
-//     expect(isArray(arrayValues.empty)).toEqual(true);
-//     expect(isArray(arrayValues.strings)).toEqual(true);
-//   });
+import {
+  isArray,
+  isEmptyArray,
+  isNotArray,
+  isNotEmptyArray,
+  allValuesInArrayAreEqual,
+  allValuesInArrayAreNotEqual
+} from '../src/array-utils';
+describe('[Array utils]', () => {
+  it('isArray() should return true if the input is an array', () => {
+    expect(isArray([])).toEqual(true);
+    expect(isArray(['string'])).toEqual(true);
+  });
 
-//   it('isArray() should return false if the input is not an array', () => {
-//     expect(isArray(primitiveValues.null)).toEqual(false);
-//     expect(isArray(primitiveValues.unde)).toEqual(false);
-//     expect(isArray(primitiveValues.number)).toEqual(false);
-//     expect(isArray(primitiveValues.string)).toEqual(false);
-//     expect(isArray(primitiveValues.true)).toEqual(false);
-//   });
+  it('isArray() should return false if the input is not an array', () => {
+    expect(isArray(null)).toEqual(false);
+    expect(isArray(undefined)).toEqual(false);
+    expect(isArray(42)).toEqual(false);
+    expect(isArray('string')).toEqual(false);
+    expect(isArray(true)).toEqual(false);
+  });
 
-//   it('isNotArray() should return true if the input is not an array', () => {
-//     expect(isNotArray(primitiveValues.null)).toEqual(true);
-//     expect(isNotArray(primitiveValues.unde)).toEqual(true);
-//     expect(isNotArray(primitiveValues.number)).toEqual(true);
-//     expect(isNotArray(primitiveValues.string)).toEqual(true);
-//     expect(isNotArray(primitiveValues.true)).toEqual(true);
-//   });
+  it('isNotArray() should return true if the input is not an array', () => {
+    expect(isNotArray(null)).toEqual(true);
+    expect(isNotArray(undefined)).toEqual(true);
+    expect(isNotArray(42)).toEqual(true);
+    expect(isNotArray('str')).toEqual(true);
+    expect(isNotArray(true)).toEqual(true);
+  });
 
-//   it('isNotArray() should return false if the input is an array', () => {
-//     expect(isNotArray(arrayValues.empty)).toEqual(false);
-//     expect(isNotArray(arrayValues.strings)).toEqual(false);
-//   });
+  it('isNotArray() should return false if the input is an array', () => {
+    expect(isNotArray([])).toEqual(false);
+    expect(isNotArray(['string'])).toEqual(false);
+  });
 
-//   it('isEmptyArray() should return true if the incoming array is empty', () => {
-//     expect(isEmptyArray(arrayValues.empty)).toEqual(true);
-//   });
+  it('isEmptyArray() should return true if the incoming array is empty', () => {
+    expect(isEmptyArray([])).toEqual(true);
+  });
 
-//   it('isEmptyArray() should return false if the incoming array is not empty', () => {
-//     expect(isEmptyArray(arrayValues.strings)).toEqual(false);
-//   });
+  it('isEmptyArray() should return false if the incoming array is not empty', () => {
+    expect(isEmptyArray(['s'])).toEqual(false);
+  });
 
-//   it('isEmptyArray() should throw an error when argument is not an array', () => {
-//     // https://stackoverflow.com/a/4144803/9620995
-//     primitiveValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         isEmptyArray(value);
-//       }).toThrow(new Error('Argument is not an array.'));
-//     });
-//   });
+  it('isEmptyArray() should throw an error when argument is not an array', () => {
+    // https://stackoverflow.com/a/4144803/9620995
+    expect(() => {
+      //@ts-ignore
+      isEmptyArray(42);
+    }).toThrow(new Error('Argument is not an array.'));
+    expect(() => {
+      isEmptyArray(null);
+    }).toThrow(new Error('Argument is not an array.'));
+  });
 
-//   it('isNotEmptyArray() should return true when the incoming array is not empty', () => {
-//     expect(isNotEmptyArray(arrayValues.strings)).toEqual(true);
-//   });
+  it('isNotEmptyArray() should return true when the incoming array is not empty', () => {
+    expect(isNotEmptyArray(['s'])).toEqual(true);
+  });
 
-//   it('isNotEmptyArray() should return false when the incoming array is empty or is no array', () => {
-//     expect(isNotEmptyArray(arrayValues.empty)).toEqual(false);
-//   });
+  it('isNotEmptyArray() should return false when the incoming array is empty or is no array', () => {
+    expect(isNotEmptyArray([])).toEqual(false);
+  });
 
-//   it('isNotEmptyArray() should throw an error when argument is not an array', () => {
-//     primitiveValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         isNotEmptyArray(value);
-//       }).toThrow(new Error('Argument is not an array.'));
-//     });
-//   });
+  it('isNotEmptyArray() should throw an error when argument is not an array', () => {
+    expect(() => {
+      isNotEmptyArray(null);
+    }).toThrow(new Error('Argument is not an array.'));
+  });
 
-//   it('allValuesInArrayAreEqual() should return true if all arguments are the same', () => {
-//     expect(allValuesInArrayAreEqual([1, 1, 1])).toEqual(true);
-//     expect(allValuesInArrayAreEqual(['a', 'a'])).toEqual(true);
-//   });
+  it('allValuesInArrayAreEqual() should return true if all arguments are the same', () => {
+    expect(allValuesInArrayAreEqual([1, 1, 1])).toEqual(true);
+    expect(allValuesInArrayAreEqual(['a', 'a'])).toEqual(true);
+  });
 
-//   it('allValuesInArrayAreEqual() should return false if all arguments are not equal', () => {
-//     expect(allValuesInArrayAreEqual([1, 1, 2])).toEqual(false);
-//     expect(allValuesInArrayAreEqual(['a', 'b'])).toEqual(false);
-//   });
+  it('allValuesInArrayAreEqual() should return false if all arguments are not equal', () => {
+    expect(allValuesInArrayAreEqual([1, 1, 2])).toEqual(false);
+    expect(allValuesInArrayAreEqual(['a', 'b'])).toEqual(false);
+  });
 
-//   it('allValuesInArrayAreEqual() should throw an error when argument is not an array', () => {
-//     primitiveValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         allValuesInArrayAreEqual(value);
-//       }).toThrow(new Error('Arg is not an array'));
-//     });
-//     objectValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         /**
-//          * I do also want to test the case if someone really ignores every compile time hint and passes something
-//          * not 'array-like' as parameter, because of that i neet to ts-ignore
-//          */
-//         // @ts-ignore
-//         allValuesInArrayAreEqual(value);
-//       }).toThrow(new Error('Arg is not an array'));
-//     });
-//   });
+  it('allValuesInArrayAreEqual() should throw an error when argument is not an array', () => {
+    expect(() => {
+      allValuesInArrayAreEqual(null);
+    }).toThrow(new Error('Arg is not an array'));
 
-//   it('allValuesInArrayAreNotEqual() should return false if all arguments are the same', () => {
-//     expect(allValuesInArrayAreNotEqual([1, 1, 1])).toEqual(false);
-//     expect(allValuesInArrayAreNotEqual(['a', 'a'])).toEqual(false);
-//   });
+    expect(() => {
+      /**
+       * I do also want to test the case if someone really ignores every compile time hint and passes something
+       * not 'array-like' as parameter, because of that i neet to ts-ignore
+       */
+      // @ts-ignore
+      allValuesInArrayAreEqual('string');
+    }).toThrow(new Error('Arg is not an array'));
+  });
 
-//   it('allValuesInArrayAreNotEqual() should return true if all arguments are not equal', () => {
-//     expect(allValuesInArrayAreNotEqual([1, 1, 2])).toEqual(true);
-//     expect(allValuesInArrayAreNotEqual(['a', 'b'])).toEqual(true);
-//   });
+  it('allValuesInArrayAreNotEqual() should return false if all arguments are the same', () => {
+    expect(allValuesInArrayAreNotEqual([1, 1, 1])).toEqual(false);
+    expect(allValuesInArrayAreNotEqual(['a', 'a'])).toEqual(false);
+  });
 
-//   it('allValuesInArrayAreNotEqual() should throw an error when argument is not an array', () => {
-//     primitiveValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         allValuesInArrayAreNotEqual(value);
-//       }).toThrow(new Error('Arg is not an array'));
-//     });
-//     objectValuesAsArray.forEach((value) => {
-//       expect(() => {
-//         /**
-//          * I do also want to test the case if someone really ignores every compile time hint and passes something
-//          * not 'array-like' as parameter, because of that i neet to ts-ignore
-//          */
-//         // @ts-ignore
-//         allValuesInArrayAreNotEqual(value);
-//       }).toThrow(new Error('Arg is not an array'));
-//     });
-//   });
-// });
+  it('allValuesInArrayAreNotEqual() should return true if all arguments are not equal', () => {
+    expect(allValuesInArrayAreNotEqual([1, 1, 2])).toEqual(true);
+    expect(allValuesInArrayAreNotEqual(['a', 'b'])).toEqual(true);
+  });
+
+  it('allValuesInArrayAreNotEqual() should throw an error when argument is not an array', () => {
+    expect(() => {
+      //@ts-ignore
+      allValuesInArrayAreNotEqual(42);
+    }).toThrow(new Error('Arg is not an array'));
+
+    expect(() => {
+      /**
+       * I do also want to test the case if someone really ignores every compile time hint and passes something
+       * not 'array-like' as parameter, because of that i neet to ts-ignore
+       */
+      // @ts-ignore
+      allValuesInArrayAreNotEqual(42);
+    }).toThrow(new Error('Arg is not an array'));
+  });
+});
--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,96 +1,89 @@
-// import { isNotObject, isNotObjectOrArray, isObject, isObjectOrArray, isNotValid, isValid } from '../utils';
-// import {
-//   primitiveValuesAsArray,
-//   primitiveValues,
-//   objectValues,
-//   objectValuesAsArray,
-//   arrayValuesAsArray,
-//   arrayValues
-// } from '../test-utils';
-// describe('[Object utils]', () => {
-//   it('isNotObject(): should return true if input is not an object', () => {
-//     expect(isNotObject('string')).toEqual(true);
-//     expect(isNotObject(42)).toEqual(true);
-//     expect(isNotObject(undefined)).toEqual(true);
-//   });
+import { isNotObject, isNotObjectOrArray, isObject, isObjectOrArray, isNotValid, isValid } from '../src/utils';
 
-//   it('isNotObject(): should return false if input is an object', () => {
-//     expect(isNotObject({ name: 'John Doe' })).toEqual(false);
-//   });
+describe('[Object utils]', () => {
+  it('isNotObject(): should return true if input is not an object', () => {
+    expect(isNotObject('string')).toEqual(true);
+    expect(isNotObject(42)).toEqual(true);
+    expect(isNotObject(undefined)).toEqual(true);
+  });
 
-//   it('isNotObjectOrArray(): should return true if input is not an object', () => {
-//     expect(isNotObjectOrArray('string')).toEqual(true);
-//     expect(isNotObjectOrArray(42)).toEqual(true);
-//     expect(isNotObjectOrArray(undefined)).toEqual(true);
-//   });
+  it('isNotObject(): should return false if input is an object', () => {
+    expect(isNotObject({ name: 'John Doe' })).toEqual(false);
+  });
 
-//   it('isNotObjectOrArray(): should return false if input is an object', () => {
-//     expect(isNotObjectOrArray([1, 2])).toEqual(false);
-//     expect(isNotObjectOrArray({ name: 'John Doe' })).toEqual(false);
-//   });
+  it('isNotObjectOrArray(): should return true if input is not an object', () => {
+    expect(isNotObjectOrArray('string')).toEqual(true);
+    expect(isNotObjectOrArray(42)).toEqual(true);
+    expect(isNotObjectOrArray(undefined)).toEqual(true);
+  });
 
-//   it('isObject(): should return true if input is an object', () => {
-//     expect(isObject('string')).toEqual(false);
-//     expect(isObject(42)).toEqual(false);
-//     expect(isObject(undefined)).toEqual(false);
-//   });
+  it('isNotObjectOrArray(): should return false if input is an object', () => {
+    expect(isNotObjectOrArray([1, 2])).toEqual(false);
+    expect(isNotObjectOrArray({ name: 'John Doe' })).toEqual(false);
+  });
 
-//   it('isObject(): should return false if input is not an object', () => {
-//     expect(isObject({ name: 'John Doe' })).toEqual(true);
-//   });
+  it('isObject(): should return true if input is an object', () => {
+    expect(isObject('string')).toEqual(false);
+    expect(isObject(42)).toEqual(false);
+    expect(isObject(undefined)).toEqual(false);
+  });
 
-//   it('isObjectOrArray(): should return false if input is not an object', () => {
-//     expect(isObjectOrArray('string')).toEqual(false);
-//     expect(isObjectOrArray(42)).toEqual(false);
-//     expect(isObjectOrArray(undefined)).toEqual(false);
-//   });
+  it('isObject(): should return false if input is not an object', () => {
+    expect(isObject({ name: 'John Doe' })).toEqual(true);
+  });
 
-//   it('isObjectOrArray(): should return true if input is an object', () => {
-//     expect(isObjectOrArray([1, 2])).toEqual(true);
-//     expect(isObjectOrArray({ name: 'John Doe' })).toEqual(true);
-//   });
+  it('isObjectOrArray(): should return false if input is not an object', () => {
+    expect(isObjectOrArray('string')).toEqual(false);
+    expect(isObjectOrArray(42)).toEqual(false);
+    expect(isObjectOrArray(undefined)).toEqual(false);
+  });
 
-//   it('isValid() should return true when the argument is a valid value', () => {
-//     expect(isValid(primitiveValues.number)).toEqual(true);
-//     expect(isValid(primitiveValues.string)).toEqual(true);
-//     expect(isValid(primitiveValues.true)).toEqual(true);
-//     expect(isValid(primitiveValues.false)).toEqual(true);
+  it('isObjectOrArray(): should return true if input is an object', () => {
+    expect(isObjectOrArray([1, 2])).toEqual(true);
+    expect(isObjectOrArray({ name: 'John Doe' })).toEqual(true);
+  });
 
-//     expect(isValid(objectValues.empty)).toEqual(true);
-//     expect(isValid(objectValues.any)).toEqual(true);
+  it('isValid() should return true when the argument is a valid value', () => {
+    expect(isValid(42)).toEqual(true);
+    expect(isValid('string')).toEqual(true);
+    expect(isValid(true)).toEqual(true);
+    expect(isValid(false)).toEqual(true);
 
-//     expect(isValid(arrayValues.empty)).toEqual(true);
-//     expect(isValid(arrayValues.strings)).toEqual(true);
-//     expect(isValid(arrayValues.numbers)).toEqual(true);
-//     expect(isValid(arrayValues.boolean)).toEqual(true);
-//     expect(isValid(arrayValues.null)).toEqual(true);
-//     expect(isValid(arrayValues.undefined)).toEqual(true);
-//   });
+    expect(isValid({})).toEqual(true);
+    expect(isValid({ name: 'something' })).toEqual(true);
 
-//   it('isValid() should return false when the argument is not a valid value', () => {
-//     expect(isValid(primitiveValues.null)).toEqual(false);
-//     expect(isValid(primitiveValues.unde)).toEqual(false);
-//   });
+    expect(isValid([])).toEqual(true);
+    expect(isValid(['string'])).toEqual(true);
+    expect(isValid([42])).toEqual(true);
+    expect(isValid([true, false])).toEqual(true);
+    expect(isValid([null])).toEqual(true);
+    expect(isValid([undefined])).toEqual(true);
+  });
 
-//   it('isNotValid() should return true when the argument is not a valid value', () => {
-//     expect(isNotValid(primitiveValues.null)).toEqual(true);
-//     expect(isNotValid(primitiveValues.unde)).toEqual(true);
-//   });
+  it('isValid() should return false when the argument is not a valid value', () => {
+    expect(isValid(null)).toEqual(false);
+    expect(isValid(undefined)).toEqual(false);
+  });
 
-//   it('isNotValid() should return false when the argument is a valid value', () => {
-//     expect(isNotValid(primitiveValues.number)).toEqual(false);
-//     expect(isNotValid(primitiveValues.string)).toEqual(false);
-//     expect(isNotValid(primitiveValues.true)).toEqual(false);
-//     expect(isNotValid(primitiveValues.false)).toEqual(false);
+  it('isNotValid() should return true when the argument is not a valid value', () => {
+    expect(isNotValid(null)).toEqual(true);
+    expect(isNotValid(undefined)).toEqual(true);
+  });
 
-//     expect(isNotValid(objectValues.empty)).toEqual(false);
-//     expect(isNotValid(objectValues.any)).toEqual(false);
+  it('isNotValid() should return false when the argument is a valid value', () => {
+    expect(isNotValid(42)).toEqual(false);
+    expect(isNotValid('strng')).toEqual(false);
+    expect(isNotValid(true)).toEqual(false);
+    expect(isNotValid(false)).toEqual(false);
 
-//     expect(isNotValid(arrayValues.empty)).toEqual(false);
-//     expect(isNotValid(arrayValues.strings)).toEqual(false);
-//     expect(isNotValid(arrayValues.numbers)).toEqual(false);
-//     expect(isNotValid(arrayValues.boolean)).toEqual(false);
-//     expect(isNotValid(arrayValues.null)).toEqual(false);
-//     expect(isNotValid(arrayValues.undefined)).toEqual(false);
-//   });
-// });
+    expect(isNotValid({})).toEqual(false);
+    expect(isNotValid({ name: 'john' })).toEqual(false);
+
+    expect(isNotValid([])).toEqual(false);
+    expect(isNotValid(['string'])).toEqual(false);
+    expect(isNotValid([42])).toEqual(false);
+    expect(isNotValid([true, false])).toEqual(false);
+    expect(isNotValid([null])).toEqual(false);
+    expect(isNotValid([undefined])).toEqual(false);
+  });
+});
